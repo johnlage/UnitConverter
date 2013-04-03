1477bd28c089709276d581800226a53e5bf10933
John Edmund Lage <johnlage@Johns-MacBook-Pro.local>
2013-04-03 17:52:24 -0500

    Initial Commit

---
+++ UnitConverter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UnitConverter
+//
+//  Created by John Edmund Lage on 4/3/13.
+//  Copyright (c) 2013 John Edmund Lage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UCAppDelegate class]));
+    }
+}
