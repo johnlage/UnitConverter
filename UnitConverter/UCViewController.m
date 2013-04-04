6cd8e1453ab5c1f024b9d9c5dbd73a37c6c6b855
John Edmund Lage <johnlage@Johns-MacBook-Pro.local>
2013-04-04 15:39:29 -0500

    xCode Updates

--- UnitConverter/UCViewController.m
+++ UnitConverter/UCViewController.m
@@ -9,6 +9,12 @@
 #import "UCViewController.h"
 
 @interface UCViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *BinaryText;
+@property (weak, nonatomic) IBOutlet UITextField *DecimalText;
+@property (weak, nonatomic) IBOutlet UITextField *HexaText;
+- (IBAction)DecimalConvert:(id)sender;
+- (IBAction)BinaryConvert:(id)sender;
+- (IBAction)HexaConvert:(id)sender;
 
 @end
 
@@ -26,4 +32,22 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)DecimalConvert:(id)sender {
+}
+
+- (IBAction)BinaryConvert:(id)sender {
+    NSInteger a;
+    NSInteger b;
+    NSInteger c;
+    NSInteger d;
+    NSInteger e;
+    NSInteger BinaryNumber = [[self.BinaryText text] intValue];
+    NSUInteger BinaryLength;
+    BinaryLength = [self.BinaryText.text length];
+    scanf(%BinaryNumber,%a,%b,%c,%d,%e);
+
+}
+
+- (IBAction)HexaConvert:(id)sender {
+}
 @end
